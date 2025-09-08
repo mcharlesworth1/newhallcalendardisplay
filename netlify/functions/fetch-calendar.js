879de38dbc7cad08502cd11d9e6b2ff29f9f1621
mcharlesworth1 <m.charlesworth@newhallschool.co.uk>
2025-09-08 16:58:08 +0100

    Update fetch-calendar.js

--- netlify/functions/fetch-calendar.js
+++ netlify/functions/fetch-calendar.js
@@ -1,5 +1,5 @@
 exports.handler = async function() {
-    const CALENDAR_URL = 'https://www.gov.uk/bank-holidays/england-and-wales.ics';
+    const CALENDAR_URL = 'https://www.newhallschoolcalendar.co.uk/CalendarSync.ashx?Cal=New%20Hall%20School%20Calendar&ID=3282&A=1';
 
     try {
         const response = await fetch(CALENDAR_URL);
